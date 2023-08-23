39bc8a0a7435a4b74a44d6220e79f2f24f9edde8
Mirza@Arcturus <exquisitemirza@student.ub.ac.id>
2023-08-23 13:38:37 +0700

    ``build``: Implement clean arch on folder structuring

--- main.go
+++ main.go
@@ -2,14 +2,12 @@ package main
 
 import "github.com/KampungBudaya/Kampung-Budaya-2023-BE/app"
 
-// @title Kampung Budaya's API
-// @version 1.0
-
-// @license.name  Apache 2.0
-// @license.url   http://www.apache.org/licenses/LICENSE-2.0.html
-
-// @host localhost:8080
-// @BasePath /api
+// @title			Kampung Budaya's API
+// @version			1.0
+// @license.name	Apache 2.0
+// @license.url		http://www.apache.org/licenses/LICENSE-2.0.html
+// @host			localhost:8080
+// @BasePath		/api
 func main() {
 	if err := app.Run(); err != nil {
 		panic(err)
