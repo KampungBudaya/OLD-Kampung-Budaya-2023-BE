cc11ffd71bdbb6c9ab2f4e39f0853a3e99c36a61
Mirza@Arcturus <exquisitemirza@student.ub.ac.id>
2023-08-25 09:12:43 +0700

    ``fix``: Add missing role fk on user has roles

--- database/migration/002026_create_user_has_roles_table.up.sql
+++ database/migration/002026_create_user_has_roles_table.up.sql
@@ -6,3 +6,5 @@ CREATE TABLE IF NOT EXISTS `user_has_roles`
 );
 
 ALTER TABLE `user_has_roles` ADD CONSTRAINT `user_has_roles_fk_user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+
+ALTER TABLE `user_has_roles` ADD CONSTRAINT `user_has_roles_fk_role_id` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`);
